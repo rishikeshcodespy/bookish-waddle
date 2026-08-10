8c99a8c1e6293281b5187962a4ac0bfd4cbeb3fe
rishikeshcodespy <your-rishikeshraushan81@gmail.com>
2026-08-10 21:49:26 +0530

    created interactive voting checker app.

---
+++ voting.py
@@ -0,0 +1,7 @@
+user_input = input("Please enter your age.> ")
+age = int(user_input)
+
+if age >= 18:
+    print("You are old enough to vote.")
+else:
+    print("sorry, you are too young to vote")
